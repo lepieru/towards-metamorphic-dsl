95160fc82716002e451e8327d88d367fbdfcaee7
Pierre Le Gall <pierre@legall.io>
2016-01-18 00:06:26 +0100

    Initial commit

---
+++ init.el
@@ -0,0 +1,6 @@
+(add-to-list 'org-latex-classes '("custom" "\\documentclass{custom}"
+  ("\\section{%s}"       . "\\section*{%s}")
+  ("\\subsection{%s}"    . "\\subsection*{%s}")
+  ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+  ("\\paragraph{%s}"     . "\\paragraph*{%s}")
+  ("\\subparagraph{%s}"  . "\\subparagraph*{%s}")))
